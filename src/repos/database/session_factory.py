1e88c9982acb8d12a044eafb918770b6cae2a757
alesh <alesh@LAPTOP-SWORD17>
2026-08-11 18:36:07 +0300

    Small Improvements

--- src/repos/database/session_factory.py
+++ src/repos/database/session_factory.py
@@ -4,8 +4,8 @@ from src.config.settings import settings
 
 engine = create_async_engine(
     url=settings.db.pg_async_url,
-    pool_size=5,
-    max_overflow=5,
+    pool_size=settings.db.pool_size,
+    max_overflow=settings.db.max_overflow,
     pool_pre_ping=True
 )
 
